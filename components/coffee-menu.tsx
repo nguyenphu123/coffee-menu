3d694f1952d0b6f97668ed93536f2dac3dad41f4
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-09-09 10:38:27 +0700

    commit

--- components/coffee-menu.tsx
+++ components/coffee-menu.tsx
@@ -18,7 +18,7 @@ export default function Coffee_menu() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         This is a coffee shop!!!
         <div className="grid grid-cols-1 gap-1">
-          {menu.map((item: { name: String; price: String }, i) => (
+          {menu.map((item: { name: string; price: string }, i) => (
             <div className="grid grid-cols-2 gap-2" key={i}>
               <div>{item.name}</div>
               <div>{item.price}</div>
