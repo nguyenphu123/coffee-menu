d68d6ce1be523026f65335a58aea73e36533f758
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-09-09 10:26:27 +0700

    commit

---
+++ app/api/coffee_menu/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server";
+const coffee_menu = [
+  {
+    name: "Coffee 1",
+    price: "5$",
+    description: "",
+  },
+  {
+    name: "Coffee 2",
+    price: "6$",
+    description: "",
+  },
+  {
+    name: "Coffee 3",
+    price: "7$",
+    description: "",
+  },
+];
+export async function GET(req: Request) {
+  try {
+    return NextResponse.json(coffee_menu);
+  } catch (error) {
+    console.log("[ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ components/coffee-menu.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState, useEffect } from "react";
+export default function Coffee_menu() {
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    async function getDepartments() {
+      const data = await fetch("/api/coffee_menu", {
+        method: "GET",
+      }).then((res) => res.json());
+
+      setMenu(data);
+    }
+
+    getDepartments();
+  }, []);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        This is a coffee shop!!!
+        <div className="grid grid-cols-1 gap-1">
+          {menu.map((item: any, i) => (
+            <div className="grid grid-cols-2 gap-2" key={i}>
+              <div>{item.name}</div>
+              <div>{item.price}</div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+}
