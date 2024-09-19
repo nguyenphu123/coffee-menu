3f3237cea371da75b6c24279c4234df39f3d493f
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-09-19 17:54:43 +0700

    commit

--- components/coffee-menu.tsx
+++ components/coffee-menu.tsx
@@ -28,14 +28,16 @@ export default function Coffee_menu() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         This is a coffee shop!!!
-        <form onSubmit={onSubmit}>
+        <form>
           <input
             type="text"
             placeholder="Search..."
             name="search"
             onChange={(e) => setSearch(e.target.value)}
           />
-          <button type="submit">Search</button>
+          <button type="submit" onClick={() => onSubmit}>
+            Search
+          </button>
         </form>
         You search: {data}
         <div className="grid grid-cols-1 gap-1">
