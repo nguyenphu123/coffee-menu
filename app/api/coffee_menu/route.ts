36b7ab0c845da8db3e1143b99aa10b8370401b88
nguyenphu123 <84321531+nguyenphu123@users.noreply.github.com>
2024-09-19 19:37:58 +0700

    commit

--- app/api/coffee_menu/route.ts
+++ app/api/coffee_menu/route.ts
@@ -41,22 +41,39 @@ export async function GET() {
 export async function POST(req: Request) {
   const { coffee } = await req.json();
   try {
-    const loginOptions = {
-      method: "POST",
-      url: "https://nginx-ingress.akamai-coffee.uk/api/cafe-post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        coffee: coffee,
-      }),
-    };
-    const response = await fetch(
-      `https://nginx-ingress.akamai-coffee.uk/api/cafe-post`,
-      loginOptions
-    );
-    const result = await response.json();
-    return NextResponse.json(result);
+    if (coffee != "apple") {
+      const loginOptions = {
+        method: "POST",
+        url: "https://nginx-ingress.akamai-coffee.uk/api/cafe-post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          coffee: coffee,
+        }),
+      };
+      const response = await fetch(
+        `https://nginx-ingress.akamai-coffee.uk/api/cafe-post`,
+        loginOptions
+      );
+      const result = await response.json();
+      return NextResponse.json(result);
+    } else {
+      const loginOptions = {
+        method: "POST",
+        url: "https://nginx-ingress.akamai-coffee.uk/api/cafe-post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: coffee,
+      };
+      const response = await fetch(
+        `https://nginx-ingress.akamai-coffee.uk/api/cafe-post`,
+        loginOptions
+      );
+      const result = await response.json();
+      return NextResponse.json(result);
+    }
   } catch (error) {
     console.log("[ERROR]", error);
     return new NextResponse("Internal Error", { status: 500 });
